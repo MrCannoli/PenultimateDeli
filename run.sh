c84a696e6984bb167669fcc0f199189de7649801
Dan Fay <daniel.fay17@gmail.com>
2022-06-01 22:50:08 -0400

    Removed old price history file

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source venv/bin/activate # source the virtualenv to set new $PATH
 
-RUNFILE=polygon-price-history.py
+RUNFILE=CandlestickData/polygon-price-history.py
 echo "Running: $RUNFILE"
 python3 $RUNFILE
